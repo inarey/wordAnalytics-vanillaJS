02f3fd1157706192e82855ed8cf51e349b65eba9
inarey <thoselovelywords@gmail.com>
2025-05-27 04:46:59 +0900

    work on visual indicator

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 const textareaEl = document.querySelector('.textarea');
+const charNumberEl = document.querySelector('.stat__number--character');
+const twitterNumberEl = document.querySelector('.stat__number--twitter');
+const facebookNumberEl = document.querySelector('.stat__number--facebook');
+const wordsNumberEl = document.querySelector('.stat__number--words');
 
 textareaEl.addEventListener('input', (e) => {
-  const text = e.target.value;
+  // determine the new number of characters
+  const numberOfChars = textareaEl.value.length;
+  const twitterCharLeft = 280 - numberOfChars;
+  const facebookCharLeft = 2200 - numberOfChars;
   const words = text.split(/\s+/).filter(word => word.length > 0);
   const wordCount = words.length;
 
-  const charCount = text.length;
+  // add visual inidicator if limit is exceeded
+  if (twitterCharLeft < 0) {
+    twitterNumberEl.classList.add('stat__number--limit');
+  } else {
+    twitterNumberEl.classList.remove('stat__number--limit');
+  }
+  if (facebookCharLeft < 0) {
+    facebookNumberEl.classList.add('stat__number--limit');
+  } else {
+    facebookNumberEl.classList.remove('stat__number--limit');
+  }
 
-  document.querySelector('.stat__number--words').textContent = `Word Count: ${wordCount}`;
-  document.querySelector('.stat__number--character').textContent = `Character Count: ${charCount}`;
-})
+  // set new number
+  charNumberEl.textContent = numberOfChars;
+  twitterNumberEl.textContent = twitterCharLeft;
+  facebookNumberEl.textContent = facebookCharLeft;
+});
